99c1b8fad5fecf5d567395b24324a58e84dace1b
jinyen0531 <jinyen0531@gmail.com>
2021-05-07 10:55:36 +0800

    Finetune index - for another tutorial

--- index.js
+++ index.js
@@ -5,56 +5,10 @@ const redis = require("redis");
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.PORT || 6379;
 
-const client = redis.createClient(REDIS_PORT);
-
 const app = express();
-
-// Set reponse
-function setResponse(username, repos) {
-  return `<h2>${username} has ${repos} Github repos</h2>`;
-}
-
-// Make request to Github for data
-async function getRepos(req, res, next) {
-  try {
-    console.log("Fetching Data...");
-
-    const { username } = req.params;
-
-    const response = await fetch(`https://api.github.com/users/${username}`);
-
-    const data = await response.json();
-
-    const repos = data.public_repos;
-
-    // Set to Redis
-    client.setex(username, 3600, repos);
-
-    res.send(setResponse(username, repos));
-  } catch (error) {
-    console.log(error);
-    res.status(500);
-  }
-}
-
-// Cache middleware
-function cache(req, res, next) {
-  const { username } = req.params;
-
-  client.get(username, (err, data) => {
-    if (err) {
-      throw err;
-    }
-
-    if (data !== null) {
-      res.send(setResponse(username, data));
-    } else {
-      next();
-    }
-  });
-}
-
-app.get("/repos/:username", cache, getRepos);
+app.get("/", (req, res) => {
+  res.send("<h2>Hi there!!!!</h2>");
+});
 
 app.listen(5000, () => {
   console.log(`App listening on port ${PORT}`);
